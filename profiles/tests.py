5fb7704077853f3241438978e8aa4ae56a5e1b16
Ludovic Andrianoff <landrianoff2@gmail.com>
2023-06-18 20:21:15 +0200

    Adding tests for profiles

--- profiles/tests.py
+++ profiles/tests.py
@@ -1,3 +1,29 @@
-# from django.test import TestCase
+from django.contrib.auth.models import User
+from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+from .models import Profile
+
+
+class ProfilesTest(TestCase):
+
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username="Test_User",
+            password="test_password",
+            email="test@oc-lettings.com"
+        )
+        self.profile = Profile.objects.create(user=self.user, favorite_city="Paris")
+
+    def test_profiles_index(self):
+        response = self.client.get(reverse('profiles:index'))
+        assert response.status_code == 200
+        assert b"<title>Profiles</title>" in response.content
+
+    def test_profile_detail(self):
+        response = self.client.get(reverse('profiles:profile', args=["Test_User"]))
+        assert response.status_code == 200
+        assert b"<title>Test_User</title>" in response.content
+
+    def test_profiles_models_str(self):
+        assert str(self.profile) == self.user.username
