683243d3805d5b02a614585b1cf700116c9c523c
Ludovic Andrianoff <landrianoff2@gmail.com>
2023-06-27 15:01:58 +0200

    Adding sentry

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -1,4 +1,6 @@
 import os
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -109,3 +111,19 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+sentry_sdk.init(
+    dsn="https://b172a6ab002248bebdeb4520ce20bdc6@o4505431611670528.ingest.sentry.io/4505431616978944",
+    integrations=[
+        DjangoIntegration(),
+    ],
+
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    traces_sample_rate=1.0,
+
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True
+)
--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
 
+
+def trigger_error(request):
+    division_by_zero = 1 / 0
+
 urlpatterns = [
+    path('sentry-debug/', trigger_error),
     path('', include('home.urls')),
     path('lettings/', include('lettings.urls')),
     path('profiles/', include('profiles.urls')),
     path('admin/', admin.site.urls),
 ]
+
