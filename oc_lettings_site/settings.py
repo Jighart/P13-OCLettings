8803e02366da2be372fb087c41ba138c3cfa967e
Ludovic Andrianoff <landrianoff2@gmail.com>
2023-07-01 23:56:35 +0200

    Testing without env host

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', f'{os.getenv("HOST_URL")}']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'oc-lettings-site.azurewebsites.net']
 
 
 # Application definition
