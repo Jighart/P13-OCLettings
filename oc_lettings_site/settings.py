9497bdea77a4f5368e02f80e389f6289f6379e9a
Ludovic Andrianoff <landrianoff2@gmail.com>
2023-07-02 00:28:25 +0200

    Changing env pull method

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -12,7 +12,7 @@ load_dotenv()
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
