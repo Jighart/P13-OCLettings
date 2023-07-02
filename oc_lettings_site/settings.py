20f61013537a1998b2ec90e3f485e0bf696d401c
Ludovic Andrianoff <landrianoff2@gmail.com>
2023-07-02 13:05:43 +0200

    Adding remote host as env var

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -23,7 +23,7 @@ SECRET_KEY = env('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'oc-lettings-site.azurewebsites.net']
+ALLOWED_HOSTS = ['0.0.0.0', '127.0.0.1', env('HOST_URL')]
 
 
 # Application definition
