ee224e1891ab89425ad1151769e162ddc4d51b04
Ludovic Andrianoff <landrianoff2@gmail.com>
2023-07-02 07:04:38 +0200

    Troubleshooting deployed app startup fail

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -13,7 +13,7 @@ load_dotenv()
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', default=get_random_secret_key())
+SECRET_KEY = 'fp$9^593hsriajg$_%=5trot9g!1qa@ew(o-1@=&4%=hp46(s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
