e684cf703d65ed5b0eb4617424c9f6d8a578818d
Ludovic Andrianoff <landrianoff2@gmail.com>
2023-07-02 02:29:44 +0200

    Now with correct var env

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', default=get_random_secret_key())
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', os.getenv('SENTRY_DSN')]
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', os.getenv('HOST_URL')]
 
 
 # Application definition
